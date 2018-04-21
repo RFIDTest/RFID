c3e59a3091dbd7e21e074929d86026e9b7901eea
Leon <2470719811@qq.com>
2018-04-22 03:00:17 +0800

    修改resource.h文件，否则控件名更改无效

--- resource.h
+++ resource.h
@@ -10,6 +10,7 @@
 #define IDD_ReadCard                    129
 #define IDD_Wallet                      131
 #define IDD_BlockEdit                   133
+#define IDD_DIALOG1                     134
 #define IDC_TAB1                        1000
 #define IDC_EDIT1                       1001
 #define IDC_EDIT2                       1002
@@ -19,19 +20,33 @@
 #define IDC_RADIO2                      1006
 #define IDC_COMBO1                      1008
 #define IDC_COMBO2                      1009
+#define IDC_COMBO_walletBlock           1009
 #define IDC_EDIT3                       1011
+#define IDC_EDIT_balance                1011
 #define IDC_BUTTON3                     1012
+#define IDC_BUTTON_walletInit           1012
 #define IDC_BUTTON4                     1013
+#define IDC_BUTTON_rechange             1013
 #define IDC_BUTTON5                     1014
+#define IDC_BUTTON_balanceInquiry       1014
 #define IDC_BUTTON6                     1015
+#define IDC_BUTTON_deduction            1015
 #define IDC_EDIT4                       1016
+#define IDC_EDIT_rechange               1016
 #define IDC_EDIT5                       1017
+#define IDC_EDIT_deduction              1017
 #define IDC_BUTTON7                     1018
 #define IDC_EDIT6                       1019
+#define IDC_EDIT_state                  1019
 #define IDC_BUTTON8                     1020
 #define IDC_COMBO3                      1022
 #define IDC_EDIT7                       1024
+#define IDC_COMBO_walletSector          1026
+#define IDC_EDIT_key                    1027
+#define IDC_BUTTON_defualtKey           1028
+#define IDC_RADIO1_wallet               1029
 #define IDC_EDIT8                       1030
+#define IDC_RADIO2_wallet               1030
 #define IDC_EDIT9                       1031
 #define IDC_EDIT10                      1032
 #define IDC_EDIT11                      1033
@@ -42,9 +57,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_CONTROL_VALUE         1031
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
